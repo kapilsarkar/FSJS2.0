1e5ba4469b98d44c9dfca5b040525b014bd2812d
kapilsarkar <k.sarkar81@gmail.com>
2023-01-12 16:57:18 +0530

    javascriptfrstclassdetails

---
+++ JAVASCRIPT CLASS FILES/JS_01/second.js
@@ -0,0 +1,135 @@
+// let a=2;
+// let b=2.9;
+// console.log(a,b);
+
+// let a= "kapil";
+// let b= 'kapil';
+// console.log(a,b);
+
+// let a='true';
+// let b='false';
+// console.log(a);
+// console.log(b);
+// let a=20;
+// if(a===undefined)
+// console.log(true);
+// else
+// console.log(false);
+
+// let num=['1','2','3','4','5']
+// console.log(num);
+
+// let details=['Sachin','Tendulkar', '1','true','cricketer']
+// console.log(details);
+// let mydetails={
+//     fname:'kapil',
+//     lname:'sarkar',
+//     mobileno:'99999999'
+// };
+
+// console.log(mydetails);
+
+// let fn= 'Kapil';
+// let ln= 'Sarkar';
+// const mobno='9564685056';
+// console.log(fn,ln,mobno);
+
+// let fname='John';
+// let lname='Cena';
+// let city='US';
+// console.log(`My Details are :${fname} ${lname} ${city}`);
+
+//   let x=10;
+//   let y=20;
+//   let z=x+y;
+//   console.log(z);
+
+// let x=50;
+// let y=20;
+// let z=x-y;
+// console.log(z);
+
+// let x=20;
+// let y=30;
+// let z=x*y;
+// console.log(z);
+
+// let x=30;
+// let y=10;
+// let z=x/y;
+// console.log(z);
+
+// let x=6;
+// let y=4;
+// let z=x%y;
+// console.log(z);
+
+// let x=20;
+// let y=x++;
+// console.log(x,y);
+
+// let x=20;
+// let y=x--;
+// console.log(x,y);
+
+// let a=20;
+// let y=a;
+// console.log(y);
+
+// let y=20;
+// y+=1;
+// console.log(y);
+
+// let y=20;
+// y-=1;
+// console.log(y);
+
+// let y=20;
+// y*=1;
+// console.log(y);
+
+// let y=20;
+// y/=1;
+// console.log(y);
+
+// let y=20;
+// y%=2;
+// console.log(y);
+
+// let a=10;
+// let b='10';
+// console.log(a==b);
+
+// let x=10;
+// let y='10';
+// console.log(x===y);
+
+// let x=10;
+// console.log(x!=11);
+
+// let x=10;
+// let y=11;
+// console.log(x>y);
+
+// let x=20;
+// let y=30;
+// console.log(x<y);
+
+// let x=20;
+// console.log(x>=21);
+
+// let x=20;
+// console.log(x<=50);
+
+// let x=10;
+// let y=15;
+// console.log(x<20 && y>1);
+
+// let x=20;
+// let y=30;
+// console.log(x==10 || y==60);
+
+// let x=20;
+// let y=30;
+// console.log(!x>y);
+
