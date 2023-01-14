8e3dab503fd90e586ebbfa433ec1c3c2b63564bf
kapilsarkar <k.sarkar81@gmail.com>
2023-01-14 12:51:07 +0530

    javscriptclass2

---
+++ JAVASCRIPT CLASS FILES/JS_02/first.js
@@ -0,0 +1,10 @@
+//conditions in javascript:
+// We will start this article with conditions in javascript 
+// if else condition-
+//It is a type of condition where it will execute a block of code when the condition in the if stattement is true.If the condtion is false then the else block will be executed.
+// syuntax-
+//if (condition is true) {
+   // code is executed
+// } else {
+//     // code is executed
+//  }
