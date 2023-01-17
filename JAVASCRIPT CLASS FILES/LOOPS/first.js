80c8cce9709ed2c99ed060e7f2b1ddf08163f6ab
kapilsarkar <k.sarkar81@gmail.com>
2023-01-17 12:30:05 +0530

    loopsinjavascript

---
+++ JAVASCRIPT CLASS FILES/LOOPS/first.js
@@ -0,0 +1,85 @@
+//LOOPS IN JAVASCRIPT-
+//Loops are used in programming for the repeatattio.There are different types of loops in javascript-let us understand them
+//While Loop-
+//Syntax-
+// while ( condition){
+//     statements;
+//     //body of loop
+//  }
+
+//1.The condition appers in the starting of the loop
+//2.While Loop is also known as entry controlled loop
+//3.There is no condition at the end of the loop.
+//4.If the condition inside the loop becomes true then the code inside the loop is executed.
+//5.The condition is evaluate again and when the condition becomes false then the loop stops.
+
+//Lets understand while loop with an example-
+
+// let num=5;
+// while(num<=10)
+// {
+//     console.log('Value of num is' , num);
+//     num++;
+// }
+//Here the value of num is set to 5 and the condition is being checked wheter the num is less than or equal to 10 and as soon as the num value reaches to 10 the loop stops,here the num++ is the increment.So the output achieved is.
+//Do-While Loop-
+//syntax-
+// do {
+//     statements;
+//     // body of loop.
+//  }
+//  while( Condition );
+//1.The condition is present at the end of the loop
+//2.Do-While Loop is also known as exitcontrolled loop
+//3.There is a condition at the end of the loop.
+//4.During the first condition check of the loop if the condition is false then also the loop will be executed once.
+//5.If condition is true then the loop continues untill the loop condtion ends.
+
+//Let us understand do-while loop with an example-
+// let num=5
+// do{
+//     console.log('Value of num is ',num);
+//     num++;
+// }while(num<=10);
+//Here the output is same as the while loop because the condition are being same.
+//Difference between While Loop and Do-While Loop
+//Let us understand this with an example.
+
+// let i=11;
+// while(i<=10){
+//     console.log('Value of i is',i);
+//     i++;
+// }
+//Here the condtion does not satisfies so the condtion is not checked once so in the output there is nothing.
+
+// let j=11;
+// do{
+//     console.log('Value of j is',j);
+//     j++;
+// }while(j<=10);
+//Here also the condition becomes false but the code will go inside the loop once and it will print the value of j then as soon as the condition is checked then the loop ends.
+
+//For Loop-
+
+//Syntax-
+//for(initialization; condition; increment/derement){
+   //body of the 'for' loop
+//}
+//1.The initialization,condition checking and incrementor decrement is done at the beggining of the statement.
+//2.If the condition is not mentioned in the for loop then it will go infinite times.
+//3.The initialization is done once and it is not repeated.
+
+//Let us understand for loop with an example-
+
+// for(let i=0;i<=11;i++)
+// {
+//     console.log('Value of i is',i );
+// }
+
+//So here the value of i is printed from 0 to 11. 
+
+// for(let i=0;i<=10;i++){
+//     console.log(`${i} * ${i}=${i*i}`);
+//     console.log(`${i} + ${i}=${i+i}`);
+//     console.log(`${i} - ${i}=${i-i}`);
+// }
---
+++ JAVASCRIPT CLASS FILES/LOOPS/second.js
@@ -0,0 +1,36 @@
+// let num=5;
+// while(num<=10)
+// {
+//     console.log('Value of num is' , num);
+//     num++;
+// }
+
+// let num1=5
+// do{
+//     console.log('Value of num is ',num1);
+//     num1++;
+// }while(num1<=10);
+
+// let i=11;
+// while(i<=10){
+//     console.log('Value of i is',i);
+//     i++;
+// }
+
+
+// let j=11;
+// do{
+//     console.log('Value of j is',j);
+//     j++;
+// }while(j<=10);
+
+// for(let i=0;i<=11;i++)
+// {
+//     console.log('Value of i is',i );
+// }
+
+// for(let i=0;i<=10;i++){
+//     console.log(`${i} * ${i}=${i*i}`);
+//     console.log(`${i} + ${i}=${i+i}`);
+//     console.log(`${i} - ${i}=${i-i}`);
+// }
