bab2fbff7d00c92cdb16079a6d42759974ba328b
kapilsarkar <k.sarkar81@gmail.com>
2023-01-17 17:25:31 +0530

    arrayandabject

---
+++ JAVASCRIPT CLASS FILES/ARRAY2/first.js
@@ -0,0 +1,11 @@
+let names=['Kapil','Rahul','Sachin','Virat','Rohit','John'];
+console.log(names);
+//For finding the index i.e-2 which is sachin
+console.log(names[2]);
+//For finding the lenght of array
+console.log(names.length);
+//For printing the last name
+console.log(names[names.length-1]);
+//For Changing Value inside Array
+names[1]='Shewag';
+console.log(names);
---
+++ JAVASCRIPT CLASS FILES/ARRAY2/second.js
@@ -0,0 +1,9 @@
+let bankaccount={
+    firstname:'Kapil',
+    lastname:'Sarkar',
+    accontno:10101010,
+    balance:290000000,
+};
+console.log(bankaccount);//For printing the all thevalues in the object.
+//For printing any particular value from object
+console.log(bankaccount.balance);
