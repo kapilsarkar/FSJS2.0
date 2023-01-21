7b01ed0d3eb74091c65bf9801716dc8e59d7a4b2
kapilsarkar <k.sarkar81@gmail.com>
2023-01-21 07:28:42 +0530

    objectproperties

--- JAVASCRIPT CLASS FILES/OBJECTS/first.js
+++ JAVASCRIPT CLASS FILES/OBJECTS/first.js
@@ -6,12 +6,44 @@
 //Lets for example if we want to store name of cricketrs we will use array on thr other hand if we want to store name , matches played,runs scored by a cricketrs then object is used.
 
 //Syntax for creating object :
-//An object is created by first decalring the variable declaration then name of object after that = sign and curly {}.Here in this case an empty object is created.
+//An object is created by first decalring the variable declaration then name of object after that = sign and curly {}.Here in this case an empty object is created. An empty object is called object literal.
 //let us understand object by an example-
- const cricket = {
-    name1:'Virat Kohli',
-    matchesplayed:'100',
-    runsscored:'2000',
-    currentsataus: true
- };
- console.log(cricket);
+//  const cricket = {
+//     name1:'Virat Kohli',
+//     matchesplayed:'100',
+//     runsscored:'2000',
+//     currentsataus: true
+//  };
+//  console.log(cricket);
+
+ //Here in this case an an object is created by tha name of cricket.
+ //name1,mathchesplayed,runscored,currentstatus are the keys and viratkohli,100,2000 and true are the values of the respective keys and all the key value pairs are separated by commas.
+ //The combination of both key and values are called the property of an object.
+ // Apart from these string, number, bigint, boolean, null, undefine, symbol all are objects in javascript.
+  
+ //Creating object using a construvtor-
+
+ const kapil =new Object()
+
+ kapil1.country='India'
+ kapil1.religion='Hindu'
+ console.log('kapil1');
+
+ // Acessing properties of an object -
+
+ //There are two ways by which the properties of an object can be accessed.
+ //1. Dot Notation-
+ //To get the value of a property from an object, we can use a dot (.). The syntax is object.property
+//  const details = {
+//    fname:'Kapil',
+//    lname:'Sarkar',
+//    coutntry :'India'
+//  };
+//  console.log(details.fname);
+//  console.log(details.lname);
+//  console.log(details.coutntry);
+
+ //2. Using Bracket Notation-
+ //We can also get the value of a property using [ ]. 
+
+//  console.log(details['fname']);
--- JAVASCRIPT CLASS FILES/OBJECTS/second.js
+++ JAVASCRIPT CLASS FILES/OBJECTS/second.js
@@ -1,7 +1,18 @@
-const cricket = {
-    name1:'Virat Kohli',
-    matchesplayed:'100',
-    runsscored:'2000',
-    currentsataus: true
- };
- console.log(cricket);
+// const cricket = {
+//     name1:'Virat Kohli',
+//     matchesplayed:'100',
+//     runsscored:'2000',
+//     currentsataus: true
+//  };
+//  console.log(cricket);
+
+
+//  const details = {
+//     fname:'Kapil',
+//     lname:'Sarkar',
+//     coutntry :'India'
+//   };
+//   console.log(details.fname);
+//   console.log(details.lname);
+//   console.log(details.coutntry);
+//console.log(details['fname']);
