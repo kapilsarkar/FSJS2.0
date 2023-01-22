f0964975667a7ff3ff7a2808d6c52b2dce997fa3
kapilsarkar <k.sarkar81@gmail.com>
2023-01-22 12:22:48 +0530

    jsassignmentfirsttwo

---
+++ JAVASCRIPT ASSIGNMENTS/01/variable.js
@@ -0,0 +1,16 @@
+// 01. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+
+//  Vriables with data type declaration
+let fname='Kpail Sarkar'; //string
+let learning=true; //boolean
+let govtjob=undefined;//undefined
+let privatejob=null;//null
+
+
+//Display of value with their data type
+
+console.log('Tha data type of:'+ fname + 'is :' + typeof fname);
+console.log('Tha data type of:'+ learning + 'is :' + typeof learning);
+console.log('Tha data type of:'+ govtjob + 'is :' + typeof govtjob);
+console.log('Tha data type of:'+ privatejob + 'is :' + typeof privatejob);
---
+++ JAVASCRIPT ASSIGNMENTS/02/variable.js
@@ -0,0 +1,10 @@
+// 02. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+function info(){
+    let firstname='Kpail';
+    let lastname='Sarkar';
+    let martialstatus='Unmarried';
+    let country='India';
+    let age='29';
+    console.log(`My name is :${firstname} ${lastname} My Martial Status is : ${martialstatus} My country is : ${country} My age is : ${age}`);
+}info();
+
