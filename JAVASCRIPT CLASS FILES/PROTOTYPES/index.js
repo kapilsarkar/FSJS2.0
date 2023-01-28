c192095aec7deb008ed355489c52eb7ce8743636
kapilsarkar <k.sarkar81@gmail.com>
2023-01-28 12:52:24 +0530

    jsprototypesstart

---
+++ JAVASCRIPT CLASS FILES/PROTOTYPES/index.js
@@ -0,0 +1,2 @@
+//Prototypes in java Script-
+//A Prototype is an object, which JavaScript assigns to the [[Prototype]] property of an object when it creates it. Every function includes a prototype object by default.
