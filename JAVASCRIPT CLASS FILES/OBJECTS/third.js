55367cf5a218ec7fbf96cdc6ea93254fff31042e
kapilsarkar <k.sarkar81@gmail.com>
2023-02-05 06:41:27 +0530

    methodsindieobjectdescription

--- JAVASCRIPT CLASS FILES/OBJECTS/third.js
+++ JAVASCRIPT CLASS FILES/OBJECTS/third.js
@@ -3,7 +3,7 @@
 let mydetails={
     fname:"Kapil",
     lname:"Sarkar",
-    skills:[],
+    skills:[], //It is an emplty array
     skillsknown: function(skillname){  //Declaring a method inside object
         this.skills.push(skillname);
 
@@ -15,3 +15,8 @@ let mydetails={
 console.log(mydetails);
 mydetails.skillsknown("JAVASCRIPT");
 console.log(mydetails.getskills());
+
+//skillsknown is a key and then a function is declared with parameter skills name
+//Putting values indie object method- Here mydetails is an object  and skillsknown is an method and JAVASCRIPT is  a parameter.
+//getskills is a method which contains fname,,lname and skills count. 
+//Arrow function is not allowed inside a method.
