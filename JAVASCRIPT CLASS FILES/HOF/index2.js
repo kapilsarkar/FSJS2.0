5e0cd62c07c47973e342518ad25b1c1280db0ef4
kapilsarkar <k.sarkar81@gmail.com>
2023-02-09 13:20:46 +0530

    destructuring

---
+++ JAVASCRIPT CLASS FILES/HOF/index2.js
@@ -0,0 +1,9 @@
+//Destructuring-
+const value=["Kapil","Sarkar","Asansol"];
+let [fname, lname, address] = value;
+console.log(fname,lname,address);
+
+
+const arr=[1,2,3];
+let [val1,  ,val2]=arr;
+console.log(val1,val2);
