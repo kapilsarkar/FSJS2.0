d52316315878ac03f8452b920748726ade9272bb
kapilsarkar <k.sarkar81@gmail.com>
2023-02-09 17:00:06 +0530

    settimeoutsettimeinterval

---
+++ JAVASCRIPT CLASS FILES/SET TIME OUT/index.js
@@ -0,0 +1,50 @@
+// SetTimeOut
+
+// setTimeout(() => {
+//   console.log("Hello FSJS 2");
+// }, 3000);
+
+// setInterval(() => {
+//   console.log("`😇`");
+// }, 1000);
+
+//settimeout()-
+//setTimeout() is a commonly used function in JavaScript. It sets a timer (a countdown set in milliseconds) for the execution of a callback function, calling the function upon completion of the timer.
+
+//Settimeout Function-
+//The code inside the setTimeout runs after the user-defined timer gets completed. The timer is also known as the delay before the code runs inside the setTimeout.
+
+//Syntax-
+//COPY
+//setTimeout(() => { // this is the function (arrow function)
+
+//}, timeout); // this is the delay
+
+//Here in the setTimeout, we have written a function that will execute after the delay expires. And the delay will be counted in milliseconds.
+
+//Example-
+
+// setTimeout(() => {
+//     console.log("Hello My Name is Kapil Sarkar");
+// }, 5000);
+
+//Here we are executing the function after the timer expires. That’s why the arrow function will execute after 5 seconds and print “Hello My Name is Kapil Sarkar”
+
+//Using of Setime Out-
+//o use setTimeout,
+
+// First, you need to create a function that contains the code that you want to execute.
+// This function can take one or more arguments, but the first argument must be the code to execute, and the second argument must be the amount of time to wait before executing the code (in milliseconds).
+// Once you have created your function, you can then call setTimeout😯, passing in your function as the first argument and the amount of time to wait as the second argument.
+
+//Settime Time Interval-
+//Here in this case it will print the stattemnt continouslu as the specified time
+
+//Example-
+
+// setInterval(()=> {
+//     console.log("Hello My Name is Kapil SArkar");
+// },3000);
+
+//To stopt this we have to use ctrl + c
+
