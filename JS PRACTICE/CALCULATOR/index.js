9b24b5e730a455bb64c45d391b11ab891edd8920
kapilsarkar <k.sarkar81@gmail.com>
2023-02-10 11:19:30 +0530

    practicecalculator

---
+++ JS PRACTICE/CALCULATOR/index.js
@@ -0,0 +1,17 @@
+(function(){
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+
+
+    buttons.forEach(function(button){
+        button.addEventListener('click',function(e){
+            let value = e.target.dataset.num;
+            screen.value += value;
+
+        })
+    });
+    
+
+})();
