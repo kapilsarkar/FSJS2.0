8afec0d7da47963a63f554e3d39902439a21e4d5
kapilsarkar <k.sarkar81@gmail.com>
2023-02-11 11:09:33 +0530

    spreadoperator

---
+++ JAVASCRIPT CLASS FILES/SPREAD AND REST/index.js
@@ -0,0 +1,19 @@
+//Spread And Rest In java Script--
+//In Java Script The three ... operators means Spread and Rest but these two operators are not the same.
+//We will understand the difference later in this article 
+//Spread Operator--
+// While sending data then spreading of data is known as spreading.
+//In Javascript, we use the spread operator to split up array elements or object properties.
+//Spread Operator Examples--
+//Using Array--
+//The spread operator is used to divide array elements or properties on objects so that array elements can be added/inserted into new arrays. Here's an example of its use
+
+
+//  const details=['Kapil','SArkar','Asansol','MCA','Sofware Developer'];
+//   const newdetails=[...details,'Cricket'];
+//   console.log(newdetails);
+
+
+  //Here in this case first and array is declared named details with various elementss then new variable is declared by the name newdetails and  with the help of spread operator means in this case ...details a new elements is inserted in the array. 
+
+  //Using Object--
