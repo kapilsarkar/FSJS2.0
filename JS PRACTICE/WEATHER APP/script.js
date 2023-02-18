a03a99ffe64c872ae5c22a22e7e964da003535cf
kapilsarkar <k.sarkar81@gmail.com>
2023-02-18 13:09:28 +0530

    remfileupdatedofjsparacticeandworkingonweatherapp

--- JS PRACTICE/WEATHER APP/script.js
+++ JS PRACTICE/WEATHER APP/script.js
@@ -1,12 +1,32 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '199da717e7msh416936e94a512adp1685abjsnac37eb0abacd',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
-};
+const searchInput = document.querySelector(".search_input");
+const searchForm = document.querySelector(".search");
+const temp = document.querySelector(".temp");
+const city = document.querySelector(".weather_location");
+const description = document.querySelector(".weather_description");
+const weathertIcon = document.querySelector(".weather_icon img");
+const weather = document.querySelector(".weather");
+const error = document.querySelector(".error");
 
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+const apiCall = async () => {
+	try{
+		// const base_url = 'http://api.weatherapi.com/v1/current.json?key=&q=`${serchInput.value}`&aqi=no'
+
+		// const base_url = "http://api.weatherapi.com/v1/current.json?key=&q= `${searchInput.value}`&aqi=no";
+
+		const base_url= "http://api.weatherapi.com/v1/current.json?key=&q=`${serchInput.value}`&aqi=no";
+		const response =await fetch(base_url);
+
+		const data = await response.json();
+		console.log(data);
+
+	}catch(error) {
+		console.error(error);
+	}
+}
+
+const submitHandler = (e) => {
+	e.preventDefault();
+	apiCall();
+}
+
+searchForm.addEventListener("submit",submitHandler)
---
+++ JS PRACTICE/readme.md
@@ -0,0 +1,43 @@
+# VANILA VASCRIPT PROJECTS-
+
+## CALCULATOR---
+
+![IMG1](./CALCULATOR2/Screenshot%20(261).png)
+
+
+:file_folder: [FOLDER LINK](https://github.com/kapilsarkar/FSJS2.0/tree/main/JS%20PRACTICE/CALCULATOR2)
+
+## PASS MESSAGE APP---
+
+![IMG2](./PASS%20MSG/MINIMSG.png)
+
+:file_folder: [FOLDER LINK](https://github.com/kapilsarkar/FSJS2.0/tree/main/JS%20PRACTICE/PASS%20MSG)
+
+## SHOPING CART---
+
+
+![IMG3](./SHOPPING%20CART/Screenshot%20(262).png)
+
+
+:file_folder: [FOLDER LINK](https://github.com/kapilsarkar/FSJS2.0/tree/main/JS%20PRACTICE/SHOPPING%20CART)
+
+
+## STICKY NOTES---
+
+![IMG4](./STICKY%20NOTES/Screenshot%20(263).png)
+
+:file_folder: [FOLDER LINK](https://github.com/kapilsarkar/FSJS2.0/tree/main/JS%20PRACTICE/STICKY%20NOTES)
+
+## WEATHER APP---
+
+### NOTE COMPLETED : WORK IN PROGRESS
+
+
+
+
+
+
+
+
+
+
