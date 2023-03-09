e10d70b257b8b79c4d1d4511afd7a82d92eb005b
kapilsarkar <k.sarkar81@gmail.com>
2023-03-09 11:56:18 +0530

    reactpropswithminiprojectcompleted

--- REACT REVISION/part2/README.md
+++ REACT REVISION/part2/README.md
@@ -1,2 +1,6 @@
 
 # SMALL PROJECT USING COMPONETS AND PROPS
+
+## PROJECT COMPLETED
+
+![IMG1](./src/REACT1.png)                                                                                                                                                                                                                                                                                                                                                                    
--- REACT REVISION/part2/src/App.js
+++ REACT REVISION/part2/src/App.js
@@ -5,15 +5,28 @@ import Movie from './components/Movie';
 import movie from "./movie.json";
 
 function App() {
+  
+  // let login = true;
+
+  // if(login == false)
+  // {  
+  //    return <h1>You are no Allowed</h1>
+
+  // }
+  // else{
+  //   return <h1>Welcome to Movie Website</h1>
+  // }
+
   return (
     <div className="App">
      <Header />
      <div className='main'>
       {
-        movie.map((element) => {
+        movie.map((element,index) => {
         return(
           <Movie 
-          title = {element.title} 
+          key = {index}
+          title = {element.Title} 
           year = {element.Year} 
           img = {element.Poster}
           />
