d422ed1b7987aab565e12d42c15a4bf288a8615d
kapilsarkar <k.sarkar81@gmail.com>
2023-03-12 16:20:20 +0530

    reactpropsrvsn

---
+++ REACT PRACTICE2/part1/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import "../components/Header.css"
+
+function Header(){
+    return (
+       <> 
+      <h1 className='header'>REACT-PROPS</h1>
+            <h2>Hello Kapil</h2>
+       </> )
+     
+}
+
+export default Header
