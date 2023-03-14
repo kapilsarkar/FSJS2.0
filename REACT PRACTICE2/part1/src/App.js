bbbaa10a788e8538512eedf943fed66c8aede2c6
kapilsarkar <k.sarkar81@gmail.com>
2023-03-14 12:12:19 +0530

    contextapi

--- REACT PRACTICE2/part1/src/App.js
+++ REACT PRACTICE2/part1/src/App.js
@@ -7,9 +7,12 @@ import Useeffect from './components/Useeffect';
 import Useref from './components/Useref';
 import Propdrill from './components/Propdrill';
 import ChildA from './components/ChildA';
+import { createContext } from 'react';
 
+const data = createContext();
 function App() {
   const fname = "Kapil Sarkar The Great"
+  const star= "John Cena Sona Baba"
   return (
     <div className="App">
      <Header surname="Sarkar" address="Asansol"/>
@@ -19,8 +22,12 @@ function App() {
     <Useref/>
     <Propdrill/>
     <ChildA fname={fname}/>
+    <data.Provider value={star}>
+      <ChildA/>
+    </data.Provider>
     </div>
   );
 }
 
 export default App;
+export {data};
--- REACT PRACTICE2/part1/src/components/ChildC.js
+++ REACT PRACTICE2/part1/src/components/ChildC.js
@@ -1,9 +1,23 @@
 import React from "react";
+import {data} from '../App'
 
 function ChildC({fname}){
 
     return(
-        <h2>Componeent C Displaying: {fname}</h2>
+        <>
+          <h2>Componeent C Displaying: {fname}</h2>
+          <data.Consumer>
+            {
+                (star) => {
+                    return (
+                        <h2>My Favourites are :{star}</h2>
+                    )
+                }
+            }
+          </data.Consumer>
+        </>
+       
+        
     )
 
 }
