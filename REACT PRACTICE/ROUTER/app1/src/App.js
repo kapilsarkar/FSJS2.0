dea90b572349e8395be5efd76eb610664b2472a1
kapilsarkar <k.sarkar81@gmail.com>
2023-03-17 18:28:53 +0530

    reactrouterstartedlearning

---
+++ REACT PRACTICE/ROUTER/app1/src/App.js
@@ -0,0 +1,15 @@
+
+
+import './App.css';
+//ROUTES,ROUTE
+function App() {
+  return (
+    <div className="App">
+   <h1>LEARNING REACT ROUTER V6</h1>
+    </div>
+
+     
+  );
+}
+
+export default App;
