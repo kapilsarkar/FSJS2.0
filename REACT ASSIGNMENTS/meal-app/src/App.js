e469d498e74465b64f7e7eb05bbdf100f10e4725
kapilsarkar <k.sarkar81@gmail.com>
2023-03-21 06:32:36 +0530

    mealappstarted

---
+++ REACT ASSIGNMENTS/meal-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Meal from './Components/Meal';
+import  './Components/style.css';
+function App() {
+  return (
+    <div className="App">
+      <h1>MEAL-DB APP</h1>
+      <Meal/>
+    </div>
+  );
+}
+
+export default App;
---
+++ REACT ASSIGNMENTS/meal-app/src/Components/Meal.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Meal = () => {
+  return (
+    <div className='main'>
+       <div className='heading'>
+        <h1>Search Your Food Reciepe</h1>
+        <h4>LoremipsumLoremipsuLoremipsuLoremipsuLoremipsuLoremipsuLoremipsuLoremipsuLoremipsu</h4>
+       </div>
+       <div className='searchBox'>
+
+        <input type="search" className='search-bar'/>
+        </div> 
+    </div>
+  )
+}
+
+export default Meal
