4299fac70b5c46e499ec522380b5c8bc485531ea
kapilsarkar <k.sarkar81@gmail.com>
2023-03-29 07:36:01 +0530

    letvsvarvsconst

---
+++ JAVASCRIPT REVISION/PART-03/script.js
@@ -0,0 +1,19 @@
+let firstName ='Kapil';
+let lastName = 'Sarkar';
+let age =32;
+let isHappy = true;
+let a 
+ a = 'John'
+
+ const hoursInDay = 24
+let userIntro = 'Hi My  Name is '
+ + firstName  + ' '
+  + lastName + ' ' +' . I am  ' 
+  + age + ' '  +'years old'
+console.log(userIntro,a);
+console.log(hoursInDay);
+
+//console.log(details)
+console.log(details2)
+let details ='Brock Lesnar'
+var details2 ='John Cena'
