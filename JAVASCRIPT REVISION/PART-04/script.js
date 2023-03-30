0d3776a71a5381ff733d04e4200ee40455fd351d
kapilsarkar <k.sarkar81@gmail.com>
2023-03-30 07:06:20 +0530

    varableexecutionandtemporaldeadzoneinjavascript

---
+++ JAVASCRIPT REVISION/PART-04/script.js
@@ -0,0 +1,7 @@
+var firstName = 'Kapil'
+let lastName ='Sarkar'
+let age = 32
+const yearOfBirth = 1991
+console.log(firstName)
+let userIntro = 'Hi my Name is ' + ' ' + firstName + ' ' + lastName
+console.log(userIntro)
