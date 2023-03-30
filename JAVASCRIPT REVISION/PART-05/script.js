96a2b4ec69a3f24124ebc9821ce603db302c8126
kapilsarkar <k.sarkar81@gmail.com>
2023-03-30 07:50:57 +0530

    jsdialogueboxesindetail

---
+++ JAVASCRIPT REVISION/PART-05/script.js
@@ -0,0 +1,5 @@
+const isResult = alert('Welcome to Alert in JavaScript Kapil Sarkar')
+const isConfirmed = confirm('Welcome to Confirm in JavaScript Kapil Sarkar')
+
+const userInput = prompt('Enter Your Name')
+console.log(userInput)
