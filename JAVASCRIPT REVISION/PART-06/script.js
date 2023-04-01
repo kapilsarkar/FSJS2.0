a22ef62820e8c956ede885d9d48681b2d08c5b20
kapilsarkar <k.sarkar81@gmail.com>
2023-04-01 07:55:18 +0530

    stringpropertiesandtemplateliterals

--- JAVASCRIPT REVISION/PART-06/script.js
+++ JAVASCRIPT REVISION/PART-06/script.js
@@ -35,4 +35,31 @@ console.log(newMsg.includes('WWF'))
  //2. INDEXOF()-
 console.log(newMsg.indexOf('W'))
 console.log(newMsg.indexOf('z'))
+//3.REPLACE()-
+console.log(newMsg.replace('Hello','Hi'))
+//4.REPLACE ALL-
+console.log(newMsg.replaceAll('W','w'))
+ //5.CONCAT()-
+ const msg4 ='Hello Your First Name is Kapil'
+ const msg5 ='Hello Yor Last Name is Sarkar'
+ console.log(msg4.concat(' ,' , msg5))
+ //6.padstart()-
+ const last4digits ='7859'
+ console.log(last4digits.padStart(16,'*'))
+ //7.padend-
+ const first4digit ='7859'
+ console.log(first4digit.padEnd(16,'*'))
+ //8.charAt()-
+ const find = 'John Cena Brock Lesnar'
+ console.log(find.charAt(5))
+//9.charCodeAt-
+ const charectorCode = 'JOHN CENA'
+ console.log(charectorCode.charCodeAt(7))
+//10.split()-
 
+const splitmsg ='Namaste Kapil Sarkar'
+console.log(splitmsg.split(' '))
+
+
+const templateString = `Last 4 Digits of my Account Number is ${first4digit}`
+console.log(templateString)
