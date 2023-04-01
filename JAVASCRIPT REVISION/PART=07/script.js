6ed08bb5ff299c8e72bb0ff22856ea65708b8155
kapilsarkar <k.sarkar81@gmail.com>
2023-04-01 13:01:44 +0530

    mathobjectinjavascript

---
+++ JAVASCRIPT REVISION/PART=07/script.js
@@ -0,0 +1,32 @@
+//EXPONENTIAL OPERATOR --
+const exp = 5 **2
+console.log(exp)
+
+//SQUARE ROOT--
+console.log(Math.sqrt(16))
+console.log(Math.SQRT2)
+//MATH.POW--
+console.log(Math.pow(2,4))
+//MATH.FLOOR
+console.log(Math.floor(2.5))
+//MATH.CEIL
+console.log(Math.ceil(4.9))
+console.log(Math.ceil(4.1))
+//MATH.ROUND
+console.log(Math.round(2.5))
+console.log(Math.round(2.1))
+//MATH.RANDOM
+console.log(Math.random())
+console.log(Math.random()*20)
+console.log(Math.floor(Math.random()*20))
+
+//FIND A NUMBER BETWEEN 10 AND 20 AND REMOVE THE DECIMAL PART--
+
+const num1 = 10
+const num2 = 20
+const ans = Math.floor(Math.random()* (num2-num1)+num1)
+console.log(ans)
+
+//INFINITY
+console.log(8/0)
+document.write(ans)
