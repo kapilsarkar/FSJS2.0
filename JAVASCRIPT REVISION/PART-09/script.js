77c7c8ad0d56de1deb944bf8e8b437288f5a75b2
kapilsarkar <k.sarkar81@gmail.com>
2023-04-02 07:00:04 +0530

    implicitandexplicitequalityinjavascript

--- JAVASCRIPT REVISION/PART-09/script.js
+++ JAVASCRIPT REVISION/PART-09/script.js
@@ -4,3 +4,11 @@ const user2Age = 24
   // Equality or Equal to (==)
 
 console.log(user1Age==user2Age)
+
+//===(Strict Equality)
+
+const user3Age = '24'
+const user4Age = 24 
+console.log(user3Age===user4Age)
+console.log(+user3Age===user4Age)
+console.log(user3Age!==user4Age)
