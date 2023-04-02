4397b3794d56977418b4a027deb8f97f79a34093
kapilsarkar <k.sarkar81@gmail.com>
2023-04-02 06:21:38 +0530

    truthyandfalsyvaluesinjavascript

---
+++ JAVASCRIPT REVISION/PART-08/script.js
@@ -0,0 +1,7 @@
+//TRUTHY VALUES--
+const num1 = Boolean(19)
+console.log(num1)
+//FALSY VALUES--
+const num2 = Boolean(0)
+console.log(num2)
+
---
+++ JAVASCRIPT REVISION/PART-09/script.js
@@ -0,0 +1,6 @@
+const user1Age = 18
+const user2Age = 24
+//Comparison Operator
+  // Equality or Equal to (==)
+
+console.log(user1Age==user2Age)
