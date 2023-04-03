a8b22c9c4c76ea729e0e7b58a624d3388d4e08a2
kapilsarkar <k.sarkar81@gmail.com>
2023-04-03 10:58:46 +0530

    fileupdated

---
+++ JAVASCRIPT REVISION/PART-10/script.js
@@ -0,0 +1,12 @@
+const andResult = Boolean(0 && 2)
+console.log(andResult)
+const orResult = Boolean (0 || 2)
+console.log(orResult)
+const andResult2 = '' && 'Hello'
+console.log(andResult2)
+const orResult2 = '' || 'Hello'
+console.log(orResult2)
+const andResult3 = 'Hello' && null
+console.log(andResult3)
+const orResult4 = 'Hello' || null
+console.log(orResult4)
