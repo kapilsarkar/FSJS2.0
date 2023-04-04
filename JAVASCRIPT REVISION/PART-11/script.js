0af88b37edb6a18e63aa48dcb3989126444116ef
kapilsarkar <k.sarkar81@gmail.com>
2023-04-04 11:26:11 +0530

    switchcaseinjavascript

---
+++ JAVASCRIPT REVISION/PART-11/script.js
@@ -0,0 +1,27 @@
+//In Switch case order does not matter
+const dayNumber = 5
+ switch(dayNumber) {
+    case 0 :
+        console.log('It is Sunday Today')
+        break
+    case 1 :
+        console.log('It is Monday Today')
+        break
+    case 2 :
+        console.log('It is Tuesday Today')
+        break
+    case 3 :
+        console.log('It is Wednesday Today')
+        break
+    case 4 :
+        console.log('It is Thursday  Today')
+        break
+    case 5 :
+        console.log('It is Friday Today')
+        break
+    case 6 :
+        console.log('It is Saturday Today')
+        break
+    default:
+        console.log('Please Enter a Valid Number')
+ }
