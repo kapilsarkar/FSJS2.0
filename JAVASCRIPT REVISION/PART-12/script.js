e70e01b918ea4a4e0eb925d259dc2f62485e7cd8
kapilsarkar <k.sarkar81@gmail.com>
2023-04-05 07:04:24 +0530

    ternaryoperatorinjavascript

---
+++ JAVASCRIPT REVISION/PART-12/script.js
@@ -0,0 +1,9 @@
+const userName = 5 > 10 ? 'Greater Value' : 'Lesser Value'
+console.log(userName)
+
+const gender ='f'
+const userMessage = `${gender.toLowerCase() === 'f' ? 'She' : 'He'}  is a College Student`
+console.log(userMessage)
+
+const result = 0 ? 'Kapil' : 'Sarkar'
+console.log(result)
