dcafc661f90c5e26f929816e9b565dc8f9a26489
kapilsarkar <k.sarkar81@gmail.com>
2023-04-05 17:04:34 +0530

    objectsindepth

--- JAVASCRIPT REVISION/PART-14/script.js
+++ JAVASCRIPT REVISION/PART-14/script.js
@@ -4,11 +4,36 @@ const userName1 = ''
 const userName2 = ''
 
 const user1 = {
- 
+  firstName : 'Kapil'
 }
 
+const myWrestler = 'Brock Lesnar'
 const user2 = {
-    
+    firstName :'Piku',
+    'last-Name' :'Sarkar',
+    address :{
+        city: 'Asansol',
+        town :'Burnpur',
+        pin: 713325,
+      moreDetails :{
+        hobbies :'Cricket'
+      }  
+    }
+
   }
 
 console.log(user1===user2)
+console.log(user2)
+console.log(user2.firstName) //Dot Notation
+console.log(user2["last-Name"]) //String Notation
+console.log(user2[myWrestler]) //Square Notation
+
+//For Adding New Values to Object
+user2.age = 32
+user2.address.moreDetails.superstar='Virat Kohli'
+user2['is-student'] = true
+//For Accessing Values
+console.log(user2)
+console.log(user2.address.city)
+console.log(user2.address.moreDetails.hobbies)
+console.log(user2.address.moreDetails.superstar)
