92fc5ed0f888306fde92e0d2de3864b081a59a3e
kapilsarkar <k.sarkar81@gmail.com>
2023-04-07 12:42:45 +0530

    arrayinjavascript

---
+++ JAVASCRIPT REVISION/PART-16/script.js
@@ -0,0 +1,20 @@
+const userName = "Kapil Sarkar";
+console.log(userName[2]);
+const fruitsCollection = ["Apple", "Banana", "Grapes", "Dates", "PineApple"];
+console.log(fruitsCollection[2]);
+fruitsCollection[2] = "Mango";
+
+fruitsCollection[3] = "WaterMelon";
+fruitsCollection[5] = "Orange";
+console.log(fruitsCollection);
+console.log(fruitsCollection.length);
+ const rainbowColors = ['Violet','Indigo','Blue','Green','Yellow','Orange','Red']
+ rainbowColors[rainbowColors.length] ='Pink'
+ rainbowColors.push ='Black'
+ rainbowColors.push = 'White'
+ rainbowColors.pop()
+ console.log(rainbowColors)
+
+
+ //Array Methods---
+ 
