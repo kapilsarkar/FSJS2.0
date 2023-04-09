ba7c2cb2eaf5a8232e3fa2124a553f7c23dd3800
kapilsarkar <k.sarkar81@gmail.com>
2023-04-09 12:18:22 +0530

    deepcopyvsshalowcopy

--- JAVASCRIPT REVISION/PART-17/script.js
+++ JAVASCRIPT REVISION/PART-17/script.js
@@ -16,12 +16,35 @@ const user1 = {
     lastName :"Sarkar",
 }
 
-const user2 = user1
+// const user2 = user1
 
-console.log(user2)
+// console.log(user2)
 
-user2.firstName = 'Piku'
+// user2.firstName = 'Piku'
 
-console.log(user2)
+// console.log(user2)
+// console.log(user1)
+//  const user2 = {}
+ 
+//  Object.assign(user2,user1)
+//  user2.firstName = 'Piku'
+ 
+//  console.log(user1)
+//  console.log(user2)
+
+ const user2 ={...user1}
+user2.firstName ='John Cena'
 console.log(user1)
+console.log(user2)
+
+const cricketers =['Sachin','Dravid','Kohli','Dhoni','Sehwag']
+
+const myCricketers = [...cricketers]
+
+// Object.assign(myCricketers,cricketers)
+
+myCricketers.push('Rohit')
+
+console.log(cricketers)
+console.log(myCricketers)
 
