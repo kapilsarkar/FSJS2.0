c7109f60e1af81891027040f71477a37e504bae1
kapilsarkar <k.sarkar81@gmail.com>
2023-04-09 06:44:12 +0530

    copingarrayandobject

---
+++ JAVASCRIPT REVISION/PART-17/script.js
@@ -0,0 +1,27 @@
+//Array---
+const fruits = ['Mango','Apple','Orange']
+
+const myFruits = fruits
+
+myFruits.push('Dates')
+myFruits.push('Grapes')
+
+console.log(myFruits)
+console.log(fruits)
+
+//Object---
+
+const user1 = {
+    firstName : "Kapil",
+    lastName :"Sarkar",
+}
+
+const user2 = user1
+
+console.log(user2)
+
+user2.firstName = 'Piku'
+
+console.log(user2)
+console.log(user1)
+
