f4253dbc32bd09625286498fe06a0cba3b1b6b5d
kapilsarkar <k.sarkar81@gmail.com>
2023-04-12 07:22:23 +0530

    combinedassihnmentandincrementoperator

--- JAVASCRIPT REVISION/PART-18/script.js
+++ JAVASCRIPT REVISION/PART-18/script.js
@@ -1,1 +1,19 @@
-//Compound Operator
+//Compound Operator or Combined Assignment Operator
+
+let num=5
+let num1=20
+let num2 =30
+num += 5
+num1 *= 10
+num2 **=2
+console.log(num)
+console.log(num1)
+console.log(num2)
+//Increment 
+let number1 = 5
+let number2 =5
+const newNum1 = ++number1
+const newNum2 = number2++
+
+console.log(number1 , newNum1)
+console.log(number2 , newNum2)
