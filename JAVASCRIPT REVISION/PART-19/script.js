56f86da227ead962d90214d220817d35651c0792
kapilsarkar <k.sarkar81@gmail.com>
2023-04-12 11:27:15 +0530

    initialcommit

---
+++ JAVASCRIPT REVISION/PART-19/script.js
@@ -0,0 +1,17 @@
+//While Loop
+
+let i=1;
+
+while(i<=10){
+    console.log(i)
+    i++
+}
+
+const frnds = ['Sachin','Virat','John','Rinku','Brock']
+
+
+let j=0
+while(j<=5){
+  console.log(frnds[j])
+  j++
+}
