352fc798539f2f364ded949efb8a4d936720a640
kapilsarkar <k.sarkar81@gmail.com>
2023-04-13 11:50:36 +0530

    loopsinjavascript

--- JAVASCRIPT REVISION/PART-19/script.js
+++ JAVASCRIPT REVISION/PART-19/script.js
@@ -2,7 +2,7 @@
 
 let i=1;
 
-while(i<=10){
+while(i<=5){
     console.log(i)
     i++
 }
@@ -11,7 +11,29 @@ const frnds = ['Sachin','Virat','John','Rinku','Brock']
 
 
 let j=0
-while(j<=5){
-  console.log(frnds[j])
+while(j<=frnds.length){
+  console.log(`${j+1}.${frnds[j]}`)
+ 
   j++
 }
+
+//For Loop
+console.log('For Loop Started')
+for(let k=1; k<=5; k++ ){
+  console.log(k)
+}
+console.log('For Loop Ended')
+
+const crk = ['Tendulkar','Sehwag','Kohli']
+
+for(let b =0; b <= crk.length; b++){
+  console.log(crk[b])
+}
+
+//Printing Even Numbers 0 to 100
+ for (let m =0 ; m<=50; m++){
+   if(m % 2 ===0)
+   {
+      console.log(m)
+   }
+ }
