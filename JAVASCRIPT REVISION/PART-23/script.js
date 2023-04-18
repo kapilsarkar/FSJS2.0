80c79dcf6aa88ebfa20f7584df3f98ba3f61c9d9
kapilsarkar <k.sarkar81@gmail.com>
2023-04-18 06:58:48 +0530

    hoistinginjavascript

--- JAVASCRIPT REVISION/PART-23/script.js
+++ JAVASCRIPT REVISION/PART-23/script.js
@@ -1,2 +1,16 @@
-var userName = 'Kapil Sarkar'
 console.log(userName)
+var userName = 'Kapil Sarkar'
+
+hi()
+//Function DEclaration
+function hi(){
+    console.log('Hello Kapil')
+}
+//sayhi()
+//Function Expression
+//It is also called anonymous function
+const sayhi = function(){
+    console.log('Say HI Kapil')
+}
+sayhi()
+//Both function declaration and function expression are function defination 
