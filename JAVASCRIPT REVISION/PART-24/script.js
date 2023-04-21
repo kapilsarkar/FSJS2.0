19fd80927c52abc4b85c3777bb5194fe55bd12c6
kapilsarkar <k.sarkar81@gmail.com>
2023-04-21 06:59:24 +0530

    globalscopevslocalscope

--- JAVASCRIPT REVISION/PART-24/script.js
+++ JAVASCRIPT REVISION/PART-24/script.js
@@ -1,11 +1,27 @@
 const userName = 'Kapil'
-const age = 32
-
+let age = 32
+var a = 50
 function add(){
     const x = 10
     const y = 20
-   console.log(x + y)
+    console.log(x+y)
 
 }
+
 add()
+
 console.log('Program Ended')
+console.log(window.userName)
+console.log(window.a)
+console.log(window.age)
+console.log(window.add)
+
+
+function subtract(){
+    const x = 60
+    const y = 10
+    console.log(x-y)
+
+}
+
+subtract()
