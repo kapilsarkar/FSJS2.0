638d6517596127f225905ccfc8db8ce55c85edad
kapilsarkar <k.sarkar81@gmail.com>
2023-04-24 13:17:27 +0530

    javascriptsettimeinterval

---
+++ JAVASCRIPT REVISION/PART-26/script.js
@@ -0,0 +1,13 @@
+setTimeout(
+  `
+console.log("Kapil Sarkar")
+ console.log("Hello You are welcome")`,
+  2000
+);
+//Set Time Out as Higher Order Function
+const timer = setTimeout(a,3000)
+ function a (){
+    console.log('Hello World of Cricket')
+ }
+
+ a()
