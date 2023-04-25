46d9cd039bf8060e8b753f46632d41704d932ba3
kapilsarkar <k.sarkar81@gmail.com>
2023-04-25 07:14:49 +0530

    initialcommit

--- JAVASCRIPT REVISION/PART-26/script.js
+++ JAVASCRIPT REVISION/PART-26/script.js
@@ -4,6 +4,8 @@ console.log("Kapil Sarkar")
  console.log("Hello You are welcome")`,
   2000
 );
+const timer1 = setTimeout(`console.log("Hello Brock Lesnar")`,0)
+console.log("Welcome to the Game")
 //Set Time Out as Higher Order Function
 const timer = setTimeout(a,3000)
  function a (){
---
+++ JAVASCRIPT REVISION/PART-27/script.js
@@ -0,0 +1,8 @@
+function x (){
+    var a =7
+    function y(){
+        console.log(a)
+    }
+    y()
+}
+x()
