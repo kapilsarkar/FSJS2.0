e6863d5f56a76877cae0a564fa22220dc6d33b2f
kapilsarkar <k.sarkar81@gmail.com>
2023-04-27 12:33:40 +0530

    eventloopandcallbackqueueandsynchronousvsasynchronous

--- JAVASCRIPT REVISION/PART-27/script.js
+++ JAVASCRIPT REVISION/PART-27/script.js
@@ -1,6 +1,11 @@
-console.log('Hi-1')
-for(let i=0 ; i<=5; i++){
+console.log('Hi-1')    //Synchronous Code
+
+function hello(){
+    console.log('Hello World-3')
+}
+for(let i=1 ; i<=5; i++){
     console.log(i)
 }
+setTimeout(hello,9000)  //Asynchronous code 
 console.log('Hi-2')
 
