b1508df11f3d6eb62b1a661f25aaa2568ea50a2e
kapilsarkar <k.sarkar81@gmail.com>
2023-04-27 06:54:08 +0530

    settimeoutashof

--- JAVASCRIPT REVISION/PART-26/script.js
+++ JAVASCRIPT REVISION/PART-26/script.js
@@ -4,12 +4,23 @@ console.log("Kapil Sarkar")
  console.log("Hello You are welcome")`,
   2000
 );
+
+//Set TimeOut with Zero Milli Second Time
 const timer1 = setTimeout(`console.log("Hello Brock Lesnar")`,0)
 console.log("Welcome to the Game")
+
 //Set Time Out as Higher Order Function
+
 const timer = setTimeout(a,3000)
  function a (){
     console.log('Hello World of Cricket')
  }
 
  a()
+
+
+ setTimeout(function(){
+  console.log('Hi Kapil 1')
+ },0)
+
+ console.log('Hi Kapil 2')
--- JAVASCRIPT REVISION/PART-27/script.js
+++ JAVASCRIPT REVISION/PART-27/script.js
@@ -7,4 +7,3 @@ function x (){
 }
 x()
 
-y()
