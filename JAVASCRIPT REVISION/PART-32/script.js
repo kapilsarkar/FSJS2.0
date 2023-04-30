5fe8a981e433c2258ab1372f24b50a134b3eb8aa
kapilsarkar <k.sarkar81@gmail.com>
2023-04-30 07:35:55 +0530

    forEach

---
+++ JAVASCRIPT REVISION/PART-32/script.js
@@ -0,0 +1,13 @@
+const fruits = ['banana','apple','grapes','orange','pineapple']
+
+//Using for Each Method
+
+fruits.forEach(function(nameofFruits){
+    console.log(nameofFruits)
+})
+
+//Using Arrow Function
+
+fruits.forEach( (fruit) => {
+    console.log(fruit)
+})
