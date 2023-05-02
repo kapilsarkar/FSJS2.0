0467eabd493c952d9fb4f8cb4f0ccf74e46ccb23
kapilsarkar <k.sarkar81@gmail.com>
2023-05-02 10:57:43 +0530

    mapfunction

--- JAVASCRIPT REVISION/PART-33/script.js
+++ JAVASCRIPT REVISION/PART-33/script.js
@@ -26,3 +26,36 @@ const output = arr.map((x)=>{
    
 })
 console.log(output)
+
+//filter()--
+//filter all the odd values inside array--
+
+const result = arr.filter((y)=>{
+    return y % 2 
+    
+})
+
+console.log(result)
+
+//reduce()---
+
+const arr2 = [2,10,20,15,32]
+
+function findSum(arr2){
+    let sum = 0;
+
+    for(let i =0; i< arr2.length; i++){
+        sum = sum + arr2[i]
+    }
+
+    return sum
+}
+
+console.log(findSum(arr2))
+
+const sumResult = arr2.reduce(function(acc,curr){
+    acc = acc + curr 
+    return acc
+},0)
+
+console.log(`The sum function in case of reduce is :${sumResult}`)
